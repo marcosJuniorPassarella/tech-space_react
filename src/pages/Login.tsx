bc8766cb49d8bcfa8c241e0e9e1a50a4b9dc0387
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-08-28 20:11:03 -0300

    Aula_22

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -12,20 +12,44 @@ function Login() {
       </div>
 
       <div className="flex flex-col justify-center">
-        <div className="flex flex-col justify-center align-itens-center mb-14">
+        <div className="flex flex-col justify-center itens-center mb-14">
           <h1 className="text-4xl text-orange-500 text-center font-semibold">
             Tech Space 👨‍💻
           </h1>
         </div>
 
-        <form className="max-h-[400px] w-full mx-auto bg-purple-600 p-8 rounded-lg">
+        <form className="max-w-[400px] w-full mx-auto bg-purple-600 p-8 rounded-lg">
           <h2 className="text-4xl dark:text-white font-bold text-center">
             Login
           </h2>
           <div className="flex flex-col text-white py-2">
             <label>Email</label>
-            <input type="email" className="rounded-lg mt-2 p-2 bg-purple-700 focus:bg-orange-700 focus:outline-none focus:placeholder-transparent border-2 border-purple-800 " />
+            <input
+              type="email"
+              className="rounded-lg mt-2 p-2 bg-purple-700 focus:bg-orange-700 focus:outline-none focus:placeholder-transparent border-2 border-purple-800"
+              placeholder="Digite seu email"
+            />
           </div>
+          <div className="flex flex-col text-white py-2">
+            <label>Senha</label>
+            <input
+              type="password"
+              className="rounded-lg mt-2 p-2 bg-purple-700 focus:bg-orange-700 focus:outline-none focus:placeholder-transparent border-2 border-purple-800"
+              placeholder="Digite sua senha"
+              maxLength={10}
+              minLength={6}
+            />
+          </div>
+
+          <div className="flex justify-center text-white py-2 hover:cursor-pointer hover:animate-pulse">
+            <button type="button">Criar conta</button>
+          </div>
+          <button
+            type="submit"
+            className="w-full my-5 py-2 bg-orange-500 shadow-lg enabled:hover:shadow-orange-500/40 text-white font-semibold disabled:bg-orange-400 disable:shadow-none enabled:shadow-orange-500/50"
+          >
+            Fazer Login
+          </button>
         </form>
       </div>
     </div>
