9779225950ed3d41abb968e5ce2af88fa1349520
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-08-29 20:44:55 -0300

    Aula 25

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -7,6 +7,15 @@ function Login() {
   const [loginPasswordInput, setLoginPasswordInput] = useState("");
   const [loginEmailInput, setLoginEmailInput] = useState("");
   const [isLoginFormValid, setIsLoginFormValid] = useState(true);
+  const [isSignUpFormValid, setIsSignUpFormValid] = useState(true);
+  const [signUpEmailInput, setSignUpEmailInput] = useState("");
+  const [signUpPasswordInput, setSignUpPasswordInput] = useState("");
+
+  const erroAlert = (
+    <p className="flex justify-center text-red-300">
+      Preencha os campos e tente novamente
+    </p>
+  );
 
   const handleDisplayCreateAccount = (
     event: React.MouseEvent<HTMLButtonElement, MouseEvent>
@@ -26,22 +35,14 @@ function Login() {
     setDisplaySignUp(false);
   };
 
-  const handleLoginInputEmailForm = (
-    event: React.FormEvent<HTMLInputElement>
+  const handleInputForm = (
+    event: React.FormEvent<HTMLInputElement>,
+    state: React.Dispatch<React.SetStateAction<string>>
   ) => {
     const eventTarget = event.currentTarget as HTMLInputElement;
     const eventValue = eventTarget.value;
 
-    eventValue && setLoginEmailInput(eventValue);
-  };
-
-  const handleLoginInputPasswordForm = (
-    event: React.FormEvent<HTMLInputElement>
-  ) => {
-    const eventTarget = event.currentTarget as HTMLInputElement;
-    const eventValue = eventTarget.value;
-
-    eventValue && setLoginPasswordInput(eventValue);
+    eventValue && state(eventValue);
   };
 
   const handleExecuteLogin = (
@@ -53,13 +54,26 @@ function Login() {
       ? setIsLoginFormValid(true)
       : setIsLoginFormValid(false);
 
-    console.log("DADOS DO INPUT", {
-      email: loginEmailInput,
-      password: loginPasswordInput,
-    });
-
     setLoginEmailInput("");
     setLoginPasswordInput("");
+  };
+
+  const handleExecuteSignUp = (
+    event: React.MouseEvent<HTMLFormElement, MouseEvent>
+  ) => {
+    event.preventDefault();
+
+    signUpEmailInput.trim().length > 0 && signUpPasswordInput.trim().length > 0
+      ? setIsSignUpFormValid(true)
+      : setIsSignUpFormValid(false);
+
+    console.log("DADOS DO INPUT - SIGNUP", {
+      email: signUpEmailInput,
+      password: signUpPasswordInput,
+    });
+
+    setSignUpEmailInput("");
+    setSignUpPasswordInput("");
   };
 
   return (
@@ -91,9 +105,13 @@ function Login() {
               <label>Email</label>
               <input
                 value={loginEmailInput}
-                onChange={handleLoginInputEmailForm}
+                onChange={(e) => handleInputForm(e, setLoginEmailInput)}
                 type="email"
-                className="rounded-lg mt-2 p-2 bg-purple-700 focus:bg-orange-700 focus:outline-none focus:placeholder-transparent border-2 border-purple-800"
+                className={`rounded-lg mt-2 p-2 ${
+                  isLoginFormValid
+                    ? `bg-purple-700 focus:bg-purple-800`
+                    : `bg-red-700 focus:bg-red-800`
+                }  focus:bg-orange-700 focus:outline-none focus:placeholder-transparent border-2 border-purple-800`}
                 placeholder="Digite seu email"
               />
             </div>
@@ -101,10 +119,14 @@ function Login() {
               <label>Senha</label>
               <input
                 type="password"
-                className="rounded-lg mt-2 p-2 bg-purple-700 focus:bg-orange-700 focus:outline-none focus:placeholder-transparent border-2 border-purple-800"
+                className={`rounded-lg mt-2 p-2 ${
+                  isLoginFormValid
+                    ? `bg-purple-700 focus:bg-purple-800`
+                    : `bg-red-700 focus:bg-red-800`
+                }  focus:bg-orange-700 focus:outline-none focus:placeholder-transparent border-2 border-purple-800`}
                 placeholder="Digite sua senha"
                 value={loginPasswordInput}
-                onChange={handleLoginInputPasswordForm}
+                onChange={(e) => handleInputForm(e, setLoginPasswordInput)}
                 maxLength={10}
                 minLength={6}
               />
@@ -118,8 +140,8 @@ function Login() {
                 Criar conta
               </button>
             </div>
+            {!isLoginFormValid && erroAlert}
             <button
-              disabled={!isLoginFormValid}
               type="submit"
               className="w-full my-5 py-2 bg-orange-500 shadow-lg enabled:hover:shadow-orange-500/40 text-white font-semibold disabled:bg-orange-400 disable:shadow-none enabled:shadow-orange-500/50"
             >
@@ -129,7 +151,10 @@ function Login() {
         )}
 
         {displaySignUp && (
-          <form className="max-w-[400px] w-full mx-auto bg-purple-600 p-8 px-8 rounded-lg">
+          <form
+            onSubmit={handleExecuteSignUp}
+            className="max-w-[400px] w-full mx-auto bg-purple-600 p-8 px-8 rounded-lg"
+          >
             <h2 className="text-4xl dark:text-white font-bold text-center">
               Criar conta
             </h2>
@@ -137,16 +162,28 @@ function Login() {
             <div className="flex flex-col text-white py-2">
               <label>Email</label>
               <input
+                value={signUpEmailInput}
+                onChange={(e) => handleInputForm(e, setSignUpEmailInput)}
                 type="email"
-                className="rounded-lg mt-2 p-2 bg-purple-700 focus:bg-orange-700 focus:outline-none focus:placeholder-transparent border-2 border-purple-800"
+                className={`rounded-lg mt-2 p-2 ${
+                  isSignUpFormValid
+                    ? `bg-purple-700 focus:bg-purple-800`
+                    : `bg-red-700 focus:bg-red-800`
+                }  focus:bg-orange-700 focus:outline-none focus:placeholder-transparent border-2 border-purple-800`}
                 placeholder="Digite seu email"
               />
             </div>
             <div className="flex flex-col text-white py-2">
               <label>Senha</label>
               <input
+                value={signUpPasswordInput}
+                onChange={(e) => handleInputForm(e, setSignUpPasswordInput)}
                 type="password"
-                className="rounded-lg mt-2 p-2 bg-purple-700 focus:bg-orange-700 focus:outline-none focus:placeholder-transparent border-2 border-purple-800"
+                className={`rounded-lg mt-2 p-2 ${
+                  isSignUpFormValid
+                    ? `bg-purple-700 focus:bg-purple-800`
+                    : `bg-red-700 focus:bg-red-800`
+                }  focus:bg-orange-700 focus:outline-none focus:placeholder-transparent border-2 border-purple-800`}
                 placeholder="Crie sua senha de 6 a 10 caracteres"
                 maxLength={10}
                 minLength={6}
@@ -160,6 +197,7 @@ function Login() {
                 Fazer login
               </button>
             </div>
+            {!isSignUpFormValid && erroAlert}
             <button
               type="submit"
               className="w-full my-5 py-2 bg-orange-500 shadow-lg enabled:hover:shadow-orange-500/40 text-white font-semibold rounded-lg disabled:bg-orange-400 disabled:shadow-none enabled:shadow-orange-500/50"
