db7703c3c3d8304a57e3cd25a91d26c2f8dd319e
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-09-01 17:05:19 -0300

    Aula 31

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,35 @@
-import { Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
+import { onAuthStateChanged } from "firebase/auth";
 import { RoutesPaths } from "./models/enums/routesPaths";
 import Login from "./pages/Login";
+import Feed from "./pages/Feed";
+import { auth } from "./firebase/firebaseConection";
 
 function App() {
+  const navigate = useNavigate();
+
+  const checkLogin = () => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        navigate(RoutesPaths.Feed);
+      } else {
+        navigate(RoutesPaths.Login);
+      }
+    });
+  };
+
+  // Aki é onde nós chamamos uma função quando a tela é renderizada pela primeira vez
+  useEffect(() => {
+    checkLogin();
+  }, []);
+
   return (
     <>
       <Routes>
         <Route path={RoutesPaths.Login} element={<Login />} />
+        <Route path={RoutesPaths.Feed} element={<Feed />} />
       </Routes>
       <ToastContainer
         position="top-right"
---
+++ src/pages/Feed.tsx
@@ -0,0 +1,9 @@
+function Feed() {
+  return (
+    <>
+      <h3>Hello to Feed Page</h3>
+    </>
+  );
+}
+
+export default Feed;
